3885b450bbe9df21c61a7251e5887806a391b1e7
Farshad Bolouri <bolouri.farshad@gmail.com>
2019-12-15 20:38:56 +0330

    merge the tools solved

--- python/Merge the Tools!.py
+++ python/Merge the Tools!.py
@@ -1,12 +1,16 @@
 def merge_the_tools(string, k):
-    n = len(string)
-    string.s
-    output = []
-    j = 0
-    for i in range(n // k):
-        output.append(string[i:i + k])
-    print(output)
-
+    result = [string.upper()[i:i + k] for i in range(0, len(string), k)]
+    temp_list = []
+    # [temp_list.append(x) for x in string if x not in temp_list]
+    for i in result:
+        sum = ''
+        for j in i:
+            if j not in sum:
+                sum += j
+        temp_list.append(sum)
+    result = temp_list
+    for i in result:
+        print(i)
 
 
 if __name__ == '__main__':
